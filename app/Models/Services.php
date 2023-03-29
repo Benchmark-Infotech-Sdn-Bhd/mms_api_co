88b8648d6c88a5cc38a0c9616c0493dbbfe9b76e
jegatheesh1988 <pandian.jega@gmail.com>
2023-03-29 14:48:59 +0530

    branch to service - service type mapping added

--- app/Models/Services.php
+++ app/Models/Services.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use OwenIt\Auditing\Contracts\Auditable;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Services extends Model implements Auditable
 {
@@ -19,4 +20,12 @@ class Services extends Model implements Auditable
     protected $fillable = [
         'service_name', 'status', 'created_by', 'modified_by'
     ];
+
+    /**
+     * @return BelongsTo
+     */
+    public function branch()
+    {
+        return $this->belongsTo('App\Models\Branch');
+    }
 }
