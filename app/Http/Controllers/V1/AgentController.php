d9be1f6b57b8d29d73b646e96cfca0cb3cdfa3b5
Jasmine <jasmineangel318@gmail.com>
2023-05-02 19:49:11 +0530

    Iterations - Countries, Sectors, Agent added

--- app/Http/Controllers/V1/AgentController.php
+++ app/Http/Controllers/V1/AgentController.php
@@ -171,4 +171,25 @@ class AgentController extends Controller
             return $this->sendError(['message' => $data['error']]);
         }
     }
+    /**
+     * Update the Agent status.
+     *
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function updateStatus(Request $request): JsonResponse
+    {
+        try {
+            $params = $this->getRequest($request);
+            $data = $this->agentServices->updateStatus($params);
+            if(isset($data['validate'])){
+                return $this->validationError($data['validate']); 
+            }
+            return $this->sendSuccess($data);
+        } catch (Exception $e) {
+            Log::error('Error - ' . print_r($e->getMessage(), true));
+            $data['error'] = 'Updation failed. Please retry.';
+            return $this->sendError(['message' => $data['error']]);
+        }
+    }
 }
