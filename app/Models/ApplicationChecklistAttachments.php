98641b24700be90908d1cb85e0e97452ffb57958
muralidharan.n1 <muralidharan.n1@codtesma.com>
2023-05-24 19:50:06 +0530

    application checklist attachment changes

--- app/Models/ApplicationChecklistAttachments.php
+++ app/Models/ApplicationChecklistAttachments.php
@@ -5,7 +5,6 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use App\Models\DocumentChecklist;
 use App\Models\DirectRecruitmentApplicationChecklist;
 use OwenIt\Auditing\Contracts\Auditable;
 
