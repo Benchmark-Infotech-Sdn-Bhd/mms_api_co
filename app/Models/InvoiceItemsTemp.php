7d5a833d9b7147eefa26971c7562ad99c2331a0c
muralidharan.n1 <muralidharan.n1@codtesma.com>
2023-09-19 16:57:11 +0530

    invoice items temp

---
+++ app/Models/InvoiceItemsTemp.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use OwenIt\Auditing\Contracts\Auditable;
+
+class InvoiceItemsTemp extends Model implements Auditable
+{
+    use \OwenIt\Auditing\Auditable;
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'invoice_items_temp';
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['service_id','expense_id','invoice_number','item','description','quantity','price','account','tax_rate','total_price'];
+
+}
