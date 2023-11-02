29ef8cf1fdb1ce6699a507084de7245e2d1854ae
enock.codtesma <enock@codtesma.com>
2023-11-02 15:58:48 +0530

    company api updated for logo and color

---
+++ database/migrations/2023_11_02_050956_create_company_attachments_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('company_attachments', function (Blueprint $table) {
+            // Id column
+            $table->id();
+            // file id column
+            $table->bigInteger('file_id')->unsigned()->nullable();
+            // Foreign key from company table
+            $table->foreign('file_id')->references('id')->on('company')->onDelete('cascade');
+            // Column for file name
+            $table->string('file_name', 255);
+            // Column for file type
+            $table->string('file_type', 255);
+            // Column for file url
+            $table->text('file_url')->nullable();
+            // Column for user id who created the attachment
+            $table->integer('created_by')->default(0);
+            // Column for user id who created the attachment
+            $table->integer('modified_by')->default(0);
+            // Column for attachment created and modified time
+            $table->timestamps();
+            // for softdelete
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('company_attachments');
+    }
+};
