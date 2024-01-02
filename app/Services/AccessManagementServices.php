cfb9944ec1daa1fc16f34950afd0f52bf9d6a047
valar_mathi <valarmathi@codtesma.com>
2024-01-02 12:36:09 +0530

    changes

--- app/Services/AccessManagementServices.php
+++ app/Services/AccessManagementServices.php
@@ -56,7 +56,7 @@ class AccessManagementServices
      */
     public function list($request) 
     {
-        if($request['user_type'] == Config::get('services.ROLE_TYPE_ADMIN') && is_null($request['role_id'])){
+        if($request['user_type'] == Config::get('services.ROLE_TYPE_ADMIN')){
             return $this->companyModulePermission->leftJoin('modules', 'modules.id', 'company_module_permission.module_id')
             ->where('company_module_permission.company_id', $request['company_id'])
             ->select('modules.id', 'modules.module_name', 'company_module_permission.id as role_permission_id')
