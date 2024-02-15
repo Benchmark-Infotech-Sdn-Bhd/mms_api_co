c302b0c12955027931a95733c690933133be5f7e
valar_mathi <valarmathi@codtesma.com>
2024-02-15 13:03:27 +0530

    adding notification changes

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -42,7 +42,7 @@ class Kernel extends ConsoleKernel
         $schedule->command('command:XeroGetItems '.Config::get('services.SUB_DOMAIN_DB_NAME_ONE'))->everySixHours();
         $schedule->command('command:AuditsDeleteData '.Config::get('services.SUB_DOMAIN_DB_NAME_ONE'))->dailyAt('00:01');
         $schedule->command('command:WorkerImportFailure '.Config::get('services.SUB_DOMAIN_DB_NAME_ONE'))->everyTwoMinutes()->withoutOverlapping();
-        $schedule->command('command:RenewalNotifications '.Config::get('services.SUB_DOMAIN_DB_NAME_ONE'))->dailyAt('00:01');
+        $schedule->command('command:RenewalNotifications '.Config::get('services.SUB_DOMAIN_DB_NAME_ONE'))->weekly();
         $schedule->command('command:UpdateCallingVisaExpiry '.Config::get('services.SUB_DOMAIN_DB_NAME_ONE'))->dailyAt('00:01');
         $schedule->command('command:ThirdPartyDeleteData '.Config::get('services.SUB_DOMAIN_DB_NAME_ONE'))->dailyAt('00:01');
         $schedule->command('command:InvoiceFailureResubmit '.Config::get('services.SUB_DOMAIN_DB_NAME_ONE'))->everyThirtyMinutes()->withoutOverlapping();
